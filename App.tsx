1cd9cff193933fdc033dc11ad37fac16be81a3a0
from20020516 <lofi.personal@gmail.com>
2022-04-18 21:13:22 +0900

    Add face recognition app for google glass ee2.

--- App.tsx
+++ App.tsx
@@ -1,115 +1,135 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
+import 'react-native-reanimated'
+import React, { useEffect, useState, useRef } from 'react'
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
+  ActivityIndicator,
+  BackHandler,
   View,
-} from 'react-native';
+  Text,
+  PermissionsAndroid,
+} from 'react-native'
+import { Camera, useCameraDevices } from 'react-native-vision-camera'
+import Reanimated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
+import { readFile, unlink } from 'react-native-fs'
+import Config from 'react-native-config'
+import { Buffer } from 'buffer'
+import { format } from 'date-fns'
+import { Rekognition } from 'aws-sdk'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const rekognition = new Rekognition({
+  region: 'ap-northeast-1',
+  credentials: {
+    accessKeyId: Config.AWS_ACCESS_KEY_ID,
+    secretAccessKey: Config.AWS_SECRET_ACCESS_KEY,
+  }
+})
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
+const FaceRekognition = () => {
+  const devices = useCameraDevices()
+  const device = devices.back
+  const camera = useRef<Camera>(null)
+
+  const faceRange = useSharedValue({ top: 0, left: 0, right: 0, bottom: 0 })
+  const [content, setContent] = useState<string | undefined>()
+
+  useEffect(() => {
+    let timeout = setTimeout(async () => {
+      const snapshot = await camera.current?.takeSnapshot({ quality: 100, skipMetadata: true })
+      const time = format(new Date(), 'hh:mm:ss')
+      if (snapshot) {
+        try {
+          const blob = await readFile(snapshot?.path, 'base64')
+          const results = await rekognition.recognizeCelebrities({
+            Image: {
+              Bytes: Buffer.from(blob, 'base64')
+            }
+          }).promise()
+          setContent(`${time} ${results.CelebrityFaces?.[0]?.Name ?? ''}`)
+        } catch (error) {
+          console.error(error)
+          setContent(`${time} ${(error as Error).message}`)
+        } finally {
+          await unlink(snapshot.path)
+        }
+      }
+    }, 1000)
+    return () => clearTimeout(timeout)
+  }, [content])
+
+  // const frameProcessor = useFrameProcessor(async (frame) => {
+  //   'worklet'
+  // }, [faceRange])
+
+  const boxOverlayStyle = useAnimatedStyle(() => ({
+    position: 'absolute',
+    borderWidth: 1,
+    borderColor: 'red',
+    ...faceRange.value
+  }), [faceRange])
+
+  if (!device) return <ActivityIndicator />
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Camera
+        ref={camera}
+        device={device}
+        // frameProcessor={frameProcessor}
+        // frameProcessorFps={1}
+        // preset='medium'
+        isActive={true}
+        photo={true}
+        zoom={3}
+        style={{ height: 360 }}
+      />
+      <View style={{
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        top: 260,
+        left: 0,
+        opacity: 0.7,
+        position: 'absolute',
+        width: 640,
+        height: 100,
+      }}>
+        <Text
+          style={{
+            fontSize: 50,
+            color: 'white',
+            marginLeft: 20,
+          }}
+        >{content}</Text>
+      </View>
+      <Reanimated.View style={boxOverlayStyle} />
     </View>
-  );
-};
+  )
+}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [granted, setGranted] = useState(false)
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    (async () => {
+      const isGranted =
+        await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA) === PermissionsAndroid.RESULTS.GRANTED
+      isGranted
+        ? setGranted(isGranted)
+        : BackHandler.exitApp()
+    })()
+  }, [])
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+      }}
+    >
+      {granted
+        ? (<FaceRekognition />)
+        : <></>
+      }
+    </View>
+  )
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
--- android/app/src/main/java/com/glass/MainApplication.java
+++ android/app/src/main/java/com/glass/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,6 +13,9 @@ import com.facebook.soloader.SoLoader;
 import com.glass.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.rnfs.RNFSPackage;
+import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +38,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       };
 
